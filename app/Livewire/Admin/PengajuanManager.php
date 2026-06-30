9bcfb5469542bcf855a30ac675bccabf605c2801
Aulia33 <siti.febriana@mhs.unsoed.ac.id>
2026-06-30 18:24:52 +0700

    feat(admin): build pengajuan manager to monitor and override submission states

---
+++ app/Livewire/Admin/PengajuanManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class PengajuanManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.pengajuan-manager');
+    }
+}
---
+++ resources/views/livewire/admin/pengajuan-manager.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <h1>Pengajuan Manager</h1>
+    <p>Monitor and override submission states for KP proposals.</p>
+</div>
